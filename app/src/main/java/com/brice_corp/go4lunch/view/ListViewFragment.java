b6b1ad99eac13c64281a4ca4514d09a04478fd35
Brice <donald130598@gmail.com>
2020-05-25 01:48:09 +0200

    Create listview fragment

---
+++ app/src/main/java/com/brice_corp/go4lunch/view/ListViewFragment.java
@@ -0,0 +1,20 @@
+package com.brice_corp.go4lunch.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.brice_corp.go4lunch.R;
+
+/**
+ * Created by <VOTRE-NOM> on <DATE-DU-JOUR>.
+ */
+public class ListViewFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.bottom_menu_fragment_list_view, container, false);
+    }
+}
