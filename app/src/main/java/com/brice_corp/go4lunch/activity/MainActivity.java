f8d46e282a0b53363da80a46aa5bb69e98496b80
Brice <donald130598@gmail.com>
2020-05-25 00:35:19 +0200

    Add the class in activity package

---
+++ app/src/main/java/com/brice_corp/go4lunch/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.brice_corp.go4lunch.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.brice_corp.go4lunch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
