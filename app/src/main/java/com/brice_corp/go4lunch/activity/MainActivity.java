de7f062738d92975a844bf06a6de7f45ff299384
Brice <donald130598@gmail.com>
2020-05-25 01:47:53 +0200

    Create all the menus and manage the different fragment

--- app/src/main/java/com/brice_corp/go4lunch/activity/MainActivity.java
+++ app/src/main/java/com/brice_corp/go4lunch/activity/MainActivity.java
@@ -1,16 +1,126 @@
 package com.brice_corp.go4lunch.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.brice_corp.go4lunch.R;
+import com.brice_corp.go4lunch.view.ListViewFragment;
+import com.brice_corp.go4lunch.view.MapViewFragment;
+import com.brice_corp.go4lunch.view.WorkmatesFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    //Components
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //TOOLBAR
+        mToolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        //BOTTOM MENU
+        setBottomMenu();
+
+        //NAVIGATION DRAWER
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        setNavigationDrawerMenu();
+
+        //SET THE DEFAULT FRAGMENT
+        setDefaultFragment();
+    }
+
+    //SET THE DEFAULT FRAGMENT
+    private void setDefaultFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapViewFragment()).commit();
+    }
+
+    //SET THE DEFAULT MENUS
+    private void setBottomMenu() {
+        BottomNavigationView bottomNavView = findViewById(R.id.bottom_navigation);
+        bottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment mSelectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.map_view_bottom_menu:
+                        mSelectedFragment = new MapViewFragment();
+                        break;
+                    case R.id.list_view__bottom_menu:
+                        mSelectedFragment = new ListViewFragment();
+                        break;
+                    case R.id.workmates__bottom_menu:
+                        mSelectedFragment = new WorkmatesFragment();
+                        break;
+                }
+                if (mSelectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, mSelectedFragment).commit();
+                }
+                return true;
+            }
+        });
+    }
+
+    //SET NAVIGATION MENU
+    private void setNavigationDrawerMenu() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //Set the listener for the buttons
+        NavigationView navigationView = findViewById(R.id.navigation_drawer_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.logout:
+                        //TODO lancer le dialog alert
+                        return false;
+                    case R.id.settings:
+                        return false;
+                    //TODO lancer l'activité settings
+                    case R.id.lunch:
+                        return false;
+                    //TODO lancer l'activité description restaurant
+                    default:
+                        break;
+                }
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+        toolbarButtonNavDrawer();
+    }
+
+    //Set the listener for the button
+    private void toolbarButtonNavDrawer() {
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    mDrawerLayout.openDrawer(GravityCompat.START);
+                } else {
+                    mDrawerLayout.closeDrawer(GravityCompat.END);
+                }
+            }
+        });
     }
 }
