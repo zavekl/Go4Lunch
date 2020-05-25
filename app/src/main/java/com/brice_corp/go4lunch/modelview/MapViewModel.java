8882447c6dca5b3587eb5e09552e18344ac2f228
Brice <donald130598@gmail.com>
2020-05-25 04:32:44 +0200

    Create the map view model with methods which locate the user

---
+++ app/src/main/java/com/brice_corp/go4lunch/modelview/MapViewModel.java
@@ -0,0 +1,31 @@
+package com.brice_corp.go4lunch.modelview;
+
+import android.app.Application;
+import android.location.LocationListener;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.brice_corp.go4lunch.di.MyApplication;
+import com.brice_corp.go4lunch.repository.MapRepository;
+import com.google.android.gms.location.LocationCallback;
+
+/**
+ * Created by <NIATEL Brice> on <08/04/2020>.
+ */
+public class MapViewModel extends AndroidViewModel {
+    private MapRepository mapRepository;
+
+    public MapViewModel(@NonNull Application application) {
+        super(application);
+        mapRepository = ((MyApplication) application).getContainerDependencies().getRepositoryMap();
+    }
+
+    public void startLocationUpdates(LocationCallback locationCallback) {
+        mapRepository.startLocationUpdates(locationCallback);
+    }
+
+    public void requestGPSUpdate(LocationListener locationListener) {
+        mapRepository.requestGPSUpdate(locationListener);
+    }
+}
