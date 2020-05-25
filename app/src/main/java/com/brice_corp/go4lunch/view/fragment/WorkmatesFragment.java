7263fd250a8ba9846ffd0ec448691139c0e3aa39
Brice <andro.dev.work@gmail.com>
2020-05-26 01:14:11 +0200

    Implement the RecyclerView in WorkmatesFragment

--- app/src/main/java/com/brice_corp/go4lunch/view/fragment/WorkmatesFragment.java
+++ app/src/main/java/com/brice_corp/go4lunch/view/fragment/WorkmatesFragment.java
@@ -6,8 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.brice_corp.go4lunch.R;
+import com.brice_corp.go4lunch.model.Workmates;
+import com.brice_corp.go4lunch.view.recyclerview.WorkmatesRecyclerViewAdapter;
 
 /**
  * Created by <VOTRE-NOM> on <DATE-DU-JOUR>.
@@ -15,6 +19,14 @@ import com.brice_corp.go4lunch.R;
 public class WorkmatesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.bottom_menu_fragment_workmates, container, false);
+        View view = inflater.inflate(R.layout.bottom_menu_fragment_workmates, container, false);
+
+        RecyclerView mRecyclerView = view.findViewById(R.id.workmates_recyclerview);
+
+        WorkmatesRecyclerViewAdapter adapter = new WorkmatesRecyclerViewAdapter(Workmates.getListFakesWorkmates());
+        mRecyclerView.setAdapter(adapter);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
+
+        return view;
     }
 }
