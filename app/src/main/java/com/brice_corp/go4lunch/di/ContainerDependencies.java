b4caf037e26bd6a787b5da268117d2add6058e12
Brice <andro.dev.work@gmail.com>
2020-05-27 09:15:01 +0200

    Add the FirestoreUserRepository DI

--- app/src/main/java/com/brice_corp/go4lunch/di/ContainerDependencies.java
+++ app/src/main/java/com/brice_corp/go4lunch/di/ContainerDependencies.java
@@ -2,25 +2,32 @@ package com.brice_corp.go4lunch.di;
 
 import android.content.Context;
 
+import com.brice_corp.go4lunch.repository.FirestoreUserRepository;
 import com.brice_corp.go4lunch.repository.MapRepository;
 
 /**
  * Created by <NIATEL Brice> on <16/04/2020>.
  */
 public class ContainerDependencies {
-    private Context context;
-    private MapRepository repositoryMap;
+    private Context mContext;
+    private MapRepository mMapRepository;
+    private FirestoreUserRepository mFirestoreUserRepository;
 
-    ContainerDependencies(Context context) {
-        this.context = context;
-        repositoryMap = new MapRepository(context);
+    ContainerDependencies(Context mContext) {
+        this.mContext = mContext;
+        mMapRepository = new MapRepository(mContext);
+        mFirestoreUserRepository = new FirestoreUserRepository();
     }
 
     public Context getContext() {
-        return context;
+        return mContext;
     }
 
-    public MapRepository getRepositoryMap() {
-        return repositoryMap;
+    public MapRepository getMapRepository() {
+        return mMapRepository;
+    }
+
+    public FirestoreUserRepository getFirestoreUserRepository() {
+        return mFirestoreUserRepository;
     }
 }
