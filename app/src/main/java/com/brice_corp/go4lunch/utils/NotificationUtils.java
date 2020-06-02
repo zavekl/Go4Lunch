e4f4f381c2962d5d7e826b71dfc94e8b2ac1f64c
Brice <andro.dev.work@gmail.com>
2020-06-02 18:23:00 +0200

    Create notification class

---
+++ app/src/main/java/com/brice_corp/go4lunch/utils/NotificationUtils.java
@@ -0,0 +1,77 @@
+package com.brice_corp.go4lunch.utils;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+import com.brice_corp.go4lunch.R;
+import com.brice_corp.go4lunch.activity.AuthenticationActivity;
+
+/**
+ * Created by <NIATEL BRICE> on <01/06/2020>.
+ */
+public class NotificationUtils {
+
+    private static final int NOTIFICATION_ID = 7;
+    private static final String NOTIFICATION_TAG = "rappel";
+    private Context context;
+    private NotificationManager notificationManager = null;
+
+    public NotificationUtils(Context context) {
+        this.context = context;
+    }
+
+    private void getNotificationManager() {
+        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    public void sendNotification(String m1) {
+        //  Create an Intent that will be shown when user will click on the Notification
+        Intent intent = new Intent(context, AuthenticationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        //  Create a Style for the Notification
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        inboxStyle.setBigContentTitle("Vous avez recu une notification");
+        inboxStyle.addLine(m1);
+
+        //  Create a Channel (Android 8)
+        String channelId = "fcm_default_channel";
+
+        //  Build a Notification object
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(context, channelId)
+                        .setSmallIcon(R.drawable.com_facebook_button_icon)
+                        .setContentTitle("Titre")
+                        .setContentText(m1)
+                        .setAutoCancel(true)
+                        .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                        .setContentIntent(pendingIntent)
+                        .setStyle(inboxStyle);
+
+        //  Add the Notification to the Notification Manager and show it.
+
+        while (notificationManager == null) {
+            getNotificationManager();
+        }
+
+        //  Support Version >= Android 8
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence channelName = "Rappel";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(channelId, channelName, importance);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        //  Show notification
+        notificationManager.notify(NOTIFICATION_TAG, NOTIFICATION_ID, notificationBuilder.build());
+    }
+
+
+}
