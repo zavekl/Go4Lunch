6b3812bddd1f74ff73b2341e601e744976f504c8
Brice <andro.dev.work@gmail.com>
2020-06-05 16:05:53 +0200

    Clean codes

--- app/src/main/java/com/brice_corp/go4lunch/repository/FirestoreUserRepository.java
+++ app/src/main/java/com/brice_corp/go4lunch/repository/FirestoreUserRepository.java
@@ -1,32 +1,26 @@
 package com.brice_corp.go4lunch.repository;
 
 
-import android.os.AsyncTask;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.lifecycle.MutableLiveData;
 
+import com.brice_corp.go4lunch.model.Restaurant;
 import com.brice_corp.go4lunch.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Created by <NIATEL Brice> on <27/05/2020>.
@@ -40,11 +34,12 @@ public class FirestoreUserRepository {
     private FirebaseFirestore mFirebaseFirestore;
 
     //Collections
-    private CollectionReference nameNoteRef;
+    private CollectionReference mNameNoteRef;
 
     //Constants
     private static final String USERS = "users";
     private static final String USER_NAME = "name";
+    private String CURRENT_USER_ID;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     //Methods
@@ -58,20 +53,20 @@ public class FirestoreUserRepository {
     //Initialize firestore
     private void initFirestore() {
         mFirebaseFirestore = FirebaseFirestore.getInstance();
-        nameNoteRef = mFirebaseFirestore.collection(USERS);
+        mNameNoteRef = mFirebaseFirestore.collection(USERS);
+        CURRENT_USER_ID = getCurrentUser().getUid();
     }
 
-    //Create current user in firestore database
-    public void createCurrentUserFirestore() {
-        DocumentReference mDocumentReference = mFirebaseFirestore.collection("users").document(getUser().getUid());
-        final Map<String, Object> user = new HashMap<>();
-        user.put("name", getCurrentUserName());
-        user.put("email", getCurrentUserEmail());
-
-        mDocumentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+    //Check if current user is already created in database, if not create it
+    public void checkIfUserAlreadyCreated() {
+        mNameNoteRef.document(CURRENT_USER_ID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onSuccess(Void aVoid) {
-                Log.e("FirestoreUserRepository", "onSuccess: User profile is successfully created in firestore ");
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.getResult().exists()) {
+                    Log.i("FirestoreUserRepository", "The user is already created in database");
+                } else {
+                    createCurrentUserFirestore();
+                }
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -81,26 +76,43 @@ public class FirestoreUserRepository {
         });
     }
 
+    //Create current user in firestore database
+    private void createCurrentUserFirestore() {
+        DocumentReference mDocumentReference = mNameNoteRef.document(CURRENT_USER_ID);
+
+        final Map<String, Object> user = new HashMap<>();
+        user.put("name", getUser().getName());
+        user.put("email", getUser().getmEmail());
+
+        mDocumentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.i("FirestoreUserRepository", "onSuccess: User profile is successfully created in firestore ");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("FirestoreUserRepository", "onFailure: ", e);
+            }
+        });
+
+    }
+
     //Get current user
-    private FirebaseUser getUser() {
-        return FirebaseAuth.getInstance().getCurrentUser();
+    private User getUser() {
+        FirebaseUser user = getCurrentUser();
+        //TODO lier l'ID du resto
+        Restaurant restaurant = new Restaurant("Chez Hans", "Allemand", "42 rue du Panzer", "Ouvre bientôt");
+        return new User(user.getDisplayName(), user.getEmail(), restaurant);
     }
 
     //Get name of the current user
-    private String getCurrentUserName() {
-        return getUser().getDisplayName();
-    }
-
-    //Get the email of the current user
-    private String getCurrentUserEmail() {
-        return getUser().getEmail();
+    public FirebaseUser getCurrentUser() {
+        return FirebaseAuth.getInstance().getCurrentUser();
     }
 
     //Get the call of query
     public Query getQuery() {
-        //Query firestore
-        return nameNoteRef.orderBy(USER_NAME, Query.Direction.ASCENDING);
+        return mNameNoteRef.orderBy(USER_NAME, Query.Direction.ASCENDING);
     }
-
-//TODO IMAGE
 }
