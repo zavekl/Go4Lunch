fb925ff839736836e65f1665041e76dc8004838c
Brice <andro.dev.work@gmail.com>
2020-09-19 14:29:05 +0200

    Fix key

--- app/src/main/java/com/brice_corp/go4lunch/repository/RetrofitRepository.java
+++ app/src/main/java/com/brice_corp/go4lunch/repository/RetrofitRepository.java
@@ -60,6 +60,7 @@ public class RetrofitRepository {
     //Get the restaurant information with Place API
     public LiveData<Restaurant> getRestaurantDetails(@NonNull String idRestaurant) {
         final MutableLiveData<Restaurant> restaurantDetailsResults = new MutableLiveData<>();
+        //TODO
         mApiService.getRestaurantDetails(idRestaurant, mContext.getResources().getString(R.string.map_api_key)).enqueue(
                 new Callback<Restaurant>() {
                     @Override
@@ -81,6 +82,7 @@ public class RetrofitRepository {
 
     public LiveData<NearByPlaceResults> getRestaurantListAroundUser(String latLng) {
         final MutableLiveData<NearByPlaceResults> liveData = new MutableLiveData<>();
+        //TODO
         final Call<NearByPlaceResults> call = mApiService.getRestaurantListAroundUser(latLng, RADIUS, TYPESEARCH, SENSOR, mContext.getResources().getString(R.string.map_api_key));
 
         call.enqueue(new Callback<NearByPlaceResults>() {
--- app/src/main/java/com/brice_corp/go4lunch/view/recyclerview/ListViewRestaurantRecyclerViewAdapter.java
+++ app/src/main/java/com/brice_corp/go4lunch/view/recyclerview/ListViewRestaurantRecyclerViewAdapter.java
@@ -63,6 +63,7 @@ public class ListViewRestaurantRecyclerViewAdapter extends RecyclerView.Adapter<
         holder.addressRestaurant.setText(mItemRestaurants.get(position).getAdrAddress());
 
         //PHOTO
+        //TODO
         if (mItemRestaurants.get(position).getPhotos() != null) {
             Glide.with(mContext)
                     .load("https://maps.googleapis.com/maps/api/place/photo?maxwidth=200&photoreference="
